a9ef10aabe8c6d5b84b0ca703832d9c5a04c592b
Ali <ali.cruz@me.com>
2023-07-09 12:58:16 -0600

    Avances

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCore01.Models;
+using WebCore01.Services;
+
+namespace WebCore01.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        public IActionResult Privacy()
+        {
+            ViewBag.Message = "Descripción de la APP";
+            var peliculasService = new PeliculasService();
+            var model = peliculasService.ObtenerPeliculas();
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/PeliculasService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCore01.Models;
+
+namespace WebCore01.Services
+{
+    public class PeliculasService
+    {
+        public Pelicula ObtenerPelicula()
+        {
+            return new Pelicula()
+            {
+                Titulo = "Escape Plan",
+                Duracion = 115,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 12, 5)
+            };
+        }
+
+        public List<Pelicula> ObtenerPeliculas()
+        {
+            var pelicula1 = new Pelicula()
+            {
+                Titulo = "Monsters INC",
+                Duracion = 165,
+                Pais = "USA",
+                Publicacion = new DateTime(2016, 10, 29)
+            };
+
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "Buscando a Nemo",
+                Duracion = 205,
+                Pais = "USA",
+                Publicacion = new DateTime(2011, 01, 08)
+            };
+
+            return new List<Pelicula> { pelicula1, pelicula2 };
+        }
+
+
+
+    }
+}
